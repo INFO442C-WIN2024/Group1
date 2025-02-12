9844989c2055178ce58904afad659645be3a073a
William Li <willll@Williams-MacBook-Pro-4.local>
2025-02-11 18:19:00 -0800

    Updated Browserrouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/quizweb">
+    <BrowserRouter basename="/Group1">
       <App />
     </BrowserRouter>
   </React.StrictMode>
